8f30814187a366c740c5d4c5bc9678b7dccc2ba3
ShriVarshaan <varshaanvijay@gmail.com>
2026-03-04 11:32:02 +0000

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.json({"message": "It works"})
+})
+
+app.listen(3000, () => {
+    console.log("listening on port 3000")
+})
